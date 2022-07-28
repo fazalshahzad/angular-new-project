55a700695d1a52594036e9ae38f94999a1e17c6d
fazalshahzad <shancompany322@gmail.com>
2022-07-28 13:02:59 +0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './MainPortal/about/about.component';
+import { BlogComponent } from './MainPortal/blog/blog.component';
+import { ContactComponent } from './MainPortal/contact/contact.component';
+import { HomeComponent } from './MainPortal/home/home.component';
+import { ShopComponent } from './MainPortal/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'shop',component:ShopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
