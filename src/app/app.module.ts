6043d29d8584b3ef98dbe83dfc909632cec55d71
fazalshahzad <shancompany322@gmail.com>
2022-07-28 12:48:05 +0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MainPortal/header/header.component';
+import { FooterComponent } from './MainPortal/footer/footer.component';
+import { AboutComponent } from './MainPortal/about/about.component';
+import { BlogComponent } from './MainPortal/blog/blog.component';
+import { ShopComponent } from './MainPortal/shop/shop.component';
+import { ContactComponent } from './MainPortal/contact/contact.component';
+import { HomeComponent } from './MainPortal/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    BlogComponent,
+    ShopComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
